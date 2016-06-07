28cdcd99453bfe2a30b3a4aca161e2931ee45894
TIENMD <TIENMD@DESKTOP-3AAS2K5>
2016-06-07 14:05:03 +0700

    migration test

---
+++ ProjectShop.Data/Repositores/ProductCategoryRepositores.cs
@@ -0,0 +1,27 @@
+﻿using ProjectShop.Data.Infrastructure;
+using ProjectShop.Model.Models;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+
+namespace ProjectShop.Data.Repositores
+{
+    public interface IProductCategoryRepositores
+    {
+        IEnumerable<ProductCategory> GetByAlias(string alias);
+    }
+    public class ProductCategoryRepositores : RepositoryBase<ProductCategory>, IProductCategoryRepositores
+    {
+
+
+        public ProductCategoryRepositores(IDbFactory dbFactory) : base(dbFactory)
+        {
+
+        }
+
+        public IEnumerable<ProductCategory> GetByAlias(string alias)
+        {
+            return this.DbContext.ProductCategories.Where(a => a.Alias == alias);
+        }
+    }
+}
---
+++ ProjectShop.Data/Repositores/ProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using ProjectShop.Data.Infrastructure;
+using ProjectShop.Model.Models;
+
+namespace ProjectShop.Data.Repositores
+{
+    public interface IProductRepository
+    {
+    }
+
+    public class ProductRepository : RepositoryBase<Product>
+    {
+        public ProductRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
